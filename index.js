e8b18666e7326651456cc6539116758965ed91fe
Prabu <prabu.undefined@gmail.com>
2022-08-22 15:03:20 +0530

    made code better

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-// https://api.adviceslip.com/advice
 async function getData() {
-  const res = await fetch("https://api.adviceslip.com/advice");
+  const res = await fetch("https://api.adviceslip.com/advice", {
+    cache: "no-cache",
+  });
+  if (!res.ok) throw new Error(res.statusText);
   return await res.json();
 }
 
 const adviceCard = document.querySelector("main");
 
 async function setAdvice() {
-  const { slip } = await getData();
-  adviceCard.querySelector("span").innerText = `ADVICE #${slip.id}`;
-  adviceCard.querySelector("h1").innerText = `"${slip.advice}"`;
+  try {
+    const { slip } = await getData();
+    adviceCard.querySelector("span").innerText = `ADVICE #${slip.id}`;
+    adviceCard.querySelector("h1").innerText = `"${slip.advice}"`;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 setAdvice();
